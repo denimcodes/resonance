976dff74796886bafe2d051b47ed0e36080324e4
= <=>
2026-04-13 13:18:18 +0530

    Setup clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { toast } from "sonner";
 
 export default function Home() {
   return (
-    <Button variant="destructive" onClick={() => toast.success('Hello world')}>Click me</Button>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-background">
+     <h1 className="text-2xl font-semibold">Welcome to Resonance</h1> 
+      <div className="flex items-center gap-4">
+        <OrganizationSwitcher />
+        <UserButton />
+     </div>
+    </div>
   );
 }
